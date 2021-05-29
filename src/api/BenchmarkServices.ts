22eb10d60ce99ac42c580c2545311d7fedf2ec12
Stanislas Lange <slange@scaleway.com>
2021-05-29 20:33:30 +0000

    style: format

--- src/api/BenchmarkServices.ts
+++ src/api/BenchmarkServices.ts
@@ -1,35 +1,40 @@
-import axios, {AxiosResponse} from 'axios';
+import axios, { AxiosResponse } from 'axios';
 // import useToken from "../utils/useToken";
 
 export class BenchmarkServices {
+  static async createBenchmark(
+    title: string,
+    subject: string,
+    difficulty: string,
+  ): Promise<benchmarkModel> {
+    const res: AxiosResponse<benchmarkModel> = await axios.post(
+      'http://localhost:3000/benchmarks',
+      {
+        title,
+        subject,
+        difficulty,
+      },
+      {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+          // 'Authorization': 'Bearer ' + useToken() //
+        },
+      },
+    );
+    return res.data;
+  }
 
-    static async createBenchmark(title: string, subject: string, difficulty: string): Promise<benchmarkModel> {
-        const res: AxiosResponse<benchmarkModel> = await axios.post(
-            'http://localhost:3000/benchmarks',
-            {
-                title,
-                subject,
-                difficulty,
-            }, {
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-                    // 'Authorization': 'Bearer ' + useToken() //
-                }
-            }
-        );
-        return res.data;
-    }
+  static async getAllBenchmarks(): Promise<benchmarkModel[]> {
+    return axios.get('http://localhost:3000/benchmarks').then((response) => {
+      return response.data;
+    });
+  }
 
-    static async getAllBenchmarks(): Promise<benchmarkModel[]> {
-        return axios.get('http://localhost:3000/benchmarks').then(response => {
-            return response.data;
-        });
-    }
-
-    static async getBenchmarkById(id: string): Promise<benchmarkModel> {
-        return axios.get('http://localhost:3000/benchmarks/'+ id).then(response => {
-            return response.data;
-        });
-    }
-
+  static async getBenchmarkById(id: string): Promise<benchmarkModel> {
+    return axios
+      .get('http://localhost:3000/benchmarks/' + id)
+      .then((response) => {
+        return response.data;
+      });
+  }
 }
--- src/components/benchmarks/BenchmarkModel.ts
+++ src/components/benchmarks/BenchmarkModel.ts
@@ -1,17 +1,17 @@
 // @ts-ignore
 type benchmarkModel = {
-    id: String,
-    title: String,
-    subject: String,
-    gitUrl: null,
-    createdAt: String,
-    difficulty: String,
-    creator: {
-        id: String,
-        name: String,
-        username: String,
-        email: String,
-        createdAt: String,
-        updatedAt: String
-    }
-}
+  id: String;
+  title: String;
+  subject: String;
+  gitUrl: null;
+  createdAt: String;
+  difficulty: String;
+  creator: {
+    id: String;
+    name: String;
+    username: String;
+    email: String;
+    createdAt: String;
+    updatedAt: String;
+  };
+};
