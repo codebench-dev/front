a233d6bb0943e33e5b54bffd7a13d7947d698ec2
Stanislas Lange <slange@scaleway.com>
2021-05-30 15:50:29 +0000

    feat(home): add nice and cliché landing page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,35 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
     <div>
-      {'Home ✨'}
-      <Link to="/login">Login</Link>
+      <section className="text-gray-700 ">
+        <div className="container flex flex-col px-5 py-24 mx-auto lg:items-center">
+          <div className="flex flex-col w-full mb-12 text-left lg:text-center">
+            <h2 className="mb-4 text-xs font-semibold tracking-widest text-black uppercase title-font">
+              Get up to speed...
+            </h2>
+            <h1 className="mb-6 text-2xl font-semibold tracking-tighter text-black sm:text-5xl title-font">
+              {' '}
+              Benchmark your code.{' '}
+            </h1>
+            <p className="mx-auto text-base font-medium leading-relaxed text-gray-700 lg:w-2/3">
+              Ever wondered what language would be the best suited for a
+              specific algorithm? <br />
+              Get the answer on Codebench.
+            </p>
+          </div>
+          <div className="flex justify-left lg:justify-center ">
+            <button className="px-6 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg bg-black hover:bg-gray-800 hover:to-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+              <Link to="/login">Login</Link>
+            </button>
+            <button className="px-6 py-2 ml-4 font-semibold text-black transition duration-500 ease-in-out transform border rounded-lg shadow-xl hover:bg-wickedgray-300 hover:text-white focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+              <Link to="/login">Sign up</Link>
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
