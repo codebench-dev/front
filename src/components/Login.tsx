07c3bac863353d9c0842f6b607c59d3b2d13e3d1
Gwendal <gwendou95@hotmail.fr>
2021-06-05 11:08:34 +0200

    fix: change default image with our logo

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -28,8 +28,8 @@ const Login: React.FC = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            className="mx-auto rounded-full w-1/4"
+            src="https://images2.imgbox.com/ab/88/aS4VAVYc_o.png"
             alt="Workflow"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
