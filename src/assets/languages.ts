04e175fd98487cbfd810c56e87b03192dc81c5de
Gwendal <gwendou95@hotmail.fr>
2021-07-20 00:38:41 +0200

    fix: extract languages lists into assets

---
+++ src/assets/languages.ts
@@ -0,0 +1,47 @@
+export const languagesFilterList = [
+  {
+    id: 0,
+    name: 'all',
+    avatar:
+      'https://cdn.icon-icons.com/icons2/3076/PNG/128/report_chart_graph_analytics_statistics_icon_191167.png',
+  },
+  {
+    id: 1,
+    name: 'python',
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
+  },
+  {
+    id: 2,
+    name: 'go',
+    avatar:
+      'https://blog.engineering.publicissapient.fr/wp-content/uploads/2016/10/gopher.png',
+  },
+  {
+    id: 3,
+    name: 'cpp',
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg',
+  },
+];
+
+export const languagesList = [
+  {
+    id: 1,
+    name: 'python',
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
+  },
+  {
+    id: 2,
+    name: 'go',
+    avatar:
+      'https://blog.engineering.publicissapient.fr/wp-content/uploads/2016/10/gopher.png',
+  },
+  {
+    id: 3,
+    name: 'cpp',
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg',
+  },
+];
--- src/components/leaderboard/Leaderboard.tsx
+++ src/components/leaderboard/Leaderboard.tsx
@@ -5,37 +5,13 @@ import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 import { useLeaderboardList } from '../../hooks/leaderboard';
 import { Listbox, Transition } from '@headlessui/react';
 import LeaderboardModel from './LeaderboardModel';
+import { languagesFilterList } from '../../assets/languages';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-const languages = [
-  {
-    id: 0,
-    name: 'all',
-    avatar:
-      'https://cdn.icon-icons.com/icons2/3076/PNG/128/report_chart_graph_analytics_statistics_icon_191167.png',
-  },
-  {
-    id: 1,
-    name: 'python',
-    avatar:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
-  },
-  {
-    id: 2,
-    name: 'go',
-    avatar:
-      'https://blog.engineering.publicissapient.fr/wp-content/uploads/2016/10/gopher.png',
-  },
-  {
-    id: 3,
-    name: 'cpp',
-    avatar:
-      'https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg',
-  },
-];
+const languages = languagesFilterList;
 
 interface LeaderboardProps {
   benchmarkId: string;
