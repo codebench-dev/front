c04dd6914520baab34d405f6daf6efc674b06e88
Stanislas Lange <stanislas.lange@pm.me>
2021-07-22 19:08:58 +0000

    feat: benchmark view design improvements

--- src/components/Benchmarks/Result.tsx
+++ src/components/Benchmarks/Result.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Loader from 'react-loader-spinner';
 interface LayoutProps {
   status: string;
   message: string;
@@ -10,6 +10,7 @@ interface LayoutProps {
   memUsage?: number;
   qualityScore?: number;
   lintScore?: number;
+  isLoading: boolean;
 }
 
 const Result: React.FC<LayoutProps> = ({
@@ -22,73 +23,98 @@ const Result: React.FC<LayoutProps> = ({
   memUsage,
   qualityScore,
   lintScore,
+  isLoading,
 }) => {
-  return (
-    <div className="flex dark:text-white">
-      <div className="w-1/3">
-        <ScoresComponent qualityScore={qualityScore} lintScore={lintScore} />
+  if (status !== 'done' && status !== 'error') {
+    return (
+      <div className="flex justify-center dark:text-white">
+        <Loader
+          type="ThreeDots"
+          color="#0a75ab"
+          height={100}
+          width={100}
+          timeout={10000}
+        />
       </div>
-      <div className="w-2/3 ml-2">
-        <b>Status: </b>
-        {status}
-        <div className="flex justify-between">
-          <b>Execution duration (μs): </b>
-          <div
-            className={
-              execDuration == null || execDuration === 0
-                ? 'animate-pulse h w-48 bg-gray-800 rounded-lg text-white text-cente'
-                : 'h w-48 bg-gray-800 rounded-lg text-white text-cente'
-            }
-          >
-            {execDuration === 0 ? null : execDuration}
-          </div>
+    );
+  } else {
+    return (
+      <div className="flex">
+        <div className="w-2/5">
+          <ScoresComponent
+            status={status}
+            qualityScore={qualityScore}
+            lintScore={lintScore}
+            memUsage={memUsage}
+            execDuration={execDuration}
+          />
         </div>
-        <OutputsComponent text={'Message'} value={message} />
-        <OutputsComponent text={'Error'} value={error} />
-        <OutputsComponent text={'Stdout'} value={stdout} />
-        <OutputsComponent text={'Stderr'} value={stderr} />
+        <div className="w-3/5 pl-6">
+          <OutputsComponent
+            text={'Console output'}
+            value={stdout}
+            isLoading={isLoading}
+          />
+          <OutputsComponent
+            text={'Error'}
+            value={error}
+            isLoading={isLoading}
+          />
+          <OutputsComponent
+            text={'Error output'}
+            value={stderr}
+            isLoading={isLoading}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 interface OutputsComponentProps {
   text?: string;
   value?: string;
+  isLoading: boolean;
 }
 
 const OutputsComponent: React.FC<OutputsComponentProps> = ({ text, value }) => {
-  return (
-    <div>
-      <b>{text}:</b>
-      <div
-        className={
-          value == null
-            ? 'animate-pulse h-10 w-full bg-gray-800 rounded-lg text-white'
-            : 'h-10 w-full bg-gray-800 rounded-lg text-white'
-        }
-      >
-        {value}
+  if (value) {
+    return (
+      <div>
+        <b>{text}:</b>
+        <div
+          className={'h-auto p-4 mt-2 w-full bg-gray-800 rounded-lg text-white'}
+        >
+          {value}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <div />;
+  }
 };
 
 interface ScoresComponentProps {
+  status: string | undefined;
   qualityScore: number | undefined;
   lintScore: number | undefined;
+  memUsage: number | undefined;
+  execDuration: number | undefined;
 }
 
 const ScoresComponent: React.FC<ScoresComponentProps> = ({
+  status,
   qualityScore,
   lintScore,
+  memUsage,
+  execDuration,
 }) => {
   return (
     <div className="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
       <div className="rounded-t mb-0 px-0 border-0">
         <div className="flex flex-wrap items-center px-4 py-2">
           <div className="relative w-full max-w-full flex-grow flex-1">
-            <h2 className="font-semibold text-base text-gray-900 dark:text-gray-50">
+            <h2 className="font-semibold text-lg text-gray-900 dark:text-gray-50">
               Results
             </h2>
           </div>
@@ -97,14 +123,26 @@ const ScoresComponent: React.FC<ScoresComponentProps> = ({
           <table className="items-center w-full bg-transparent border-collapse">
             <tbody>
               <tr className="text-gray-700 dark:text-gray-100">
-                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left">
+                  Exit status
+                </th>
+                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
+                  <div className="flex items-center">
+                    <span className="mr-2">
+                      {status === 'done' ? '✅' : '❌'}
+                    </span>
+                  </div>
+                </td>
+              </tr>
+              <tr className="text-gray-700 dark:text-gray-100">
+                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left">
                   Quality score
                 </th>
-                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
                   <div className="flex items-center">
                     <span className="mr-2">{qualityScore}</span>
                     <div className="relative w-full">
-                      <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
+                      <div className="overflow-hidden h-2 text-sm flex rounded bg-blue-200">
                         <div
                           style={{ width: `${qualityScore}%` }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600"
@@ -115,20 +153,40 @@ const ScoresComponent: React.FC<ScoresComponentProps> = ({
                 </td>
               </tr>
               <tr className="text-gray-700 dark:text-gray-100">
-                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left">
                   Lint score
                 </th>
-                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
                   <div className="flex items-center">
                     <span className="mr-2">{lintScore}</span>
                     <div className="relative w-full">
-                      <div className="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
+                      <div className="overflow-hidden h-2 text-sm flex rounded bg-blue-200">
                         <div
                           style={{ width: `${lintScore}%` }}
                           className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
                         />
                       </div>
                     </div>
+                  </div>
+                </td>
+              </tr>
+              <tr className="text-gray-700 dark:text-gray-100">
+                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left">
+                  Execution duration
+                </th>
+                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
+                  <div className="flex items-center">
+                    <span className="mr-2">{execDuration} μs</span>
+                  </div>
+                </td>
+              </tr>
+              <tr className="text-gray-700 dark:text-gray-100">
+                <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left">
+                  Max. memory usage
+                </th>
+                <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
+                  <div className="flex items-center">
+                    <span className="mr-2">{memUsage} KB</span>
                   </div>
                 </td>
               </tr>
