b336b7a83069ee1af316f500733d03672838adc4
Stanislas Lange <stanislas.lange@pm.me>
2021-07-28 20:45:13 +0000

    feat(profile): nice profile

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon';
 import Gravatar from 'react-gravatar';
 import { RouteComponentProps } from 'react-router-dom';
 import { useUser } from '../hooks/users';
@@ -29,17 +30,119 @@ const User = ({ match }: RouteComponentProps<UserParams>) => {
     <Page>
       <Header title="Profile" />
 
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div className="flex flex-col">
-          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <Gravatar
-                  email={profileData?.email}
-                  className="h-8 w-8 rounded-full"
-                />
-                {profileData && profileData.name}
+      <div className="w-full text-white bg-gray-50">
+        <div className="container mx-auto my-5 p-5">
+          <div className="md:flex no-wrap md:-mx-2 ">
+            {/* <!-- Left Side --> */}
+            <div className="w-full md:w-3/12 md:mx-2">
+              {/* <!-- Profile Card --> */}
+              <div className="bg-white p-3 border-t-4 border-green-400">
+                <div className="image overflow-hidden">
+                  <Gravatar
+                    size={500}
+                    email={profileData?.email}
+                    className="h-auto w-full mx-auto rounded"
+                  />
+                </div>
+                <h1 className="text-gray-900 font-bold text-xl leading-8 my-1">
+                  {profileData?.name}
+                </h1>
+                <h3 className="text-gray-600 font-lg text-semibold leading-6">
+                  Developer
+                </h3>
+                {/* <p className="text-sm text-gray-500 hover:text-gray-600 leading-6">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reprehenderit, eligendi dolorum sequi illum qui unde
+                  aspernatur non deserunt
+                </p> */}
+                <ul className="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
+                  <li className="flex items-center py-3">
+                    <span>Status</span>
+                    <span className="ml-auto">
+                      <span className="bg-green-500 py-1 px-2 rounded text-white text-sm">
+                        Active
+                      </span>
+                    </span>
+                  </li>
+                  <li className="flex items-center py-3">
+                    <span>Member since</span>
+                    <span className="ml-auto">
+                      {profileData?.createdAt
+                        ? DateTime.fromISO(profileData?.createdAt).toISODate()
+                        : ''}
+                    </span>
+                  </li>
+                </ul>
               </div>
+              {/* <!-- End of profile card --> */}
+              <div className="my-4"></div>
+            </div>
+            {/* <!-- Right Side --> */}
+            <div className="w-full md:w-9/12 mx-2 h-64">
+              {/* <!-- Profile tab --> */}
+
+              {/* <!-- Experience and education --> */}
+              <div className="bg-white p-3 shadow-sm rounded-sm">
+                <div className="grid grid-cols-2">
+                  <div>
+                    <div className="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
+                      <span className="text-green-500">
+                        <svg
+                          className="h-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                          />
+                        </svg>
+                      </span>
+                      <span className="tracking-wide">Experience</span>
+                    </div>
+                    <ul className="list-inside space-y-2">
+                      <li>
+                        <div className="text-teal-600">
+                          Owner at Her Company Inc.
+                        </div>
+                        <div className="text-gray-500 text-xs">
+                          March 2020 - Now
+                        </div>
+                      </li>
+                      <li>
+                        <div className="text-teal-600">
+                          Owner at Her Company Inc.
+                        </div>
+                        <div className="text-gray-500 text-xs">
+                          March 2020 - Now
+                        </div>
+                      </li>
+                      <li>
+                        <div className="text-teal-600">
+                          Owner at Her Company Inc.
+                        </div>
+                        <div className="text-gray-500 text-xs">
+                          March 2020 - Now
+                        </div>
+                      </li>
+                      <li>
+                        <div className="text-teal-600">
+                          Owner at Her Company Inc.
+                        </div>
+                        <div className="text-gray-500 text-xs">
+                          March 2020 - Now
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                {/* <!-- End of Experience and education grid --> */}
+              </div>
+              {/* <!-- End of profile tab --> */}
             </div>
           </div>
         </div>
